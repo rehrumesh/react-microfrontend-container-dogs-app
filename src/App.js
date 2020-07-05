2f8d633324ccc01972602bac2cd51de77ce15109
Rumesh Eranga Hapuarachchi <rehrumesh@hotmail.com>
2020-07-05 14:37:05 +0530

    Dogs app init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [dogImg, setDogImg] = useState(null);
+
+  const fetchDoggo = () => {
+    setDogImg("");
+    fetch(`https://dog.ceo/api/breeds/image/random`)
+      .then((res) => res.json())
+      .then((dogInfo) => {
+        setDogImg(dogInfo.message);
+      });
+  };
+
+  useEffect(() => {
+    if (dogImg === null) {
+      fetchDoggo();
+    }
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h3>Doggo of the day</h3>
+        <div>
+          <button onClick={() => fetchDoggo()}>New Doggo</button>
+        </div>
+        {dogImg !== "" ? (
+          <div>
+            <img src={dogImg} width="400px" alt="doggo" />
+          </div>
+        ) : (
+          <div>Loading Image</div>
+        )}
       </header>
     </div>
   );
