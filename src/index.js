ac2aa8cdd615e2f3e1e8a0e0c33a1c60653c0d7f
Rumesh Eranga Hapuarachchi <rehrumesh@hotmail.com>
2020-07-05 23:46:43 +0530

    Updated dogs app

--- src/index.js
+++ src/index.js
@@ -4,14 +4,19 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+window.renderDogs = (containerId, history) => {
+  ReactDOM.render(
+    <App history={history} />,
+    document.getElementById(containerId),
+  );
+  serviceWorker.unregister();
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+window.unmountDogs = containerId => {
+  ReactDOM.unmountComponentAtNode(document.getElementById(containerId));
+};
+
+if (!document.getElementById('Dogs-container')) {
+  ReactDOM.render(<App />, document.getElementById('root'));
+  serviceWorker.unregister();
+}
